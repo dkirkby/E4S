976a5e2be7f185cf42343290e62b73a3733eeada
David Kirkby <dkirkby@uci.edu>
2021-01-25 13:52:17 -0800

    wip

--- hello/hello_pwm.py
+++ hello/hello_pwm.py
@@ -5,7 +5,6 @@
 # https://circuitpython.readthedocs.io/en/6.0.x/shared-bindings/pulseio/index.html
 # Note that PWMOut is moving to a new pwmio module.
 import time
-import math
 import board
 import pulseio
 
@@ -18,4 +17,5 @@ PWM = pulseio.PWMOut(board.D13, frequency=FREQUENCY)
 while True:
     for duty_cycle_divisor in (2, 4, 8, 16, 32):
         PWM.duty_cycle = 0xffff // duty_cycle_divisor
+        # Could replace sleep with useful work since PWM operates in parallel.
         time.sleep(2)
