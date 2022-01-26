5c15200fe0237cfe560d62473dd843940d54d689
David Kirkby <dkirkby@uci.edu>
2022-01-26 13:58:04 -0800

    update servo example

--- hello/hello_servo.py
+++ hello/hello_servo.py
@@ -8,16 +8,19 @@
 # M4 3.3V - servo red
 # M4 D2 - servo orange
 # Optional: connect the 470uF capacitor between 3.3V and GND
+#
+# For the Pi Pico, use GP00-28 instead of D2.
 import time
 import board
-import pulseio
+import pwmio
 
 FREQUENCY = 50 # Hertz
+PERIOD_MS = 1000 / FREQUENCY
 
-PWM = pulseio.PWMOut(board.D2, frequency=FREQUENCY)
+PWM = pwmio.PWMOut(board.D2, frequency=FREQUENCY)
 
 while True:
     for pulse_duration_ms in (0.7, 1.4, 1.5, 1.6, 2.3):
-        print(pulse_duration_ms)
-        PWM.duty_cycle = int(0xffff * (pulse_duration_ms / 1000 * FREQUENCY))
+        PWM.duty_cycle = int(0xffff * (pulse_duration_ms / PERIOD_MS))
+        print(f'pulse duration={pulse_duration_ms:.1f}ms, PWM.duty_cyle=0x{PWM.duty_cycle:04x}')
         time.sleep(2)
