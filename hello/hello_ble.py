21a8b2b1589a843a41ecaee4d39c17f0f532f561
David Kirkby <dkirkby@uci.edu>
2022-02-16 13:07:07 -0800

    Update BLE example to also interpret controller packets

--- hello/hello_ble.py
+++ hello/hello_ble.py
@@ -20,6 +20,14 @@ try:
     from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
     from adafruit_ble.services.nordic import UARTService
     from adafruit_airlift.esp32 import ESP32
+    from adafruit_bluefruit_connect.packet import Packet
+    from adafruit_bluefruit_connect.color_packet import ColorPacket
+    from adafruit_bluefruit_connect.button_packet import ButtonPacket
+    from adafruit_bluefruit_connect.accelerometer_packet import AccelerometerPacket
+    from adafruit_bluefruit_connect.gyro_packet import GyroPacket
+    from adafruit_bluefruit_connect.location_packet import LocationPacket
+    from adafruit_bluefruit_connect.magnetometer_packet import MagnetometerPacket
+    from adafruit_bluefruit_connect.quaternion_packet import QuaternionPacket
 except ImportError as e:
     print(e)
     print('Are you running on an M4 Airlift?')
@@ -34,15 +42,52 @@ ble = BLERadio(adapter)
 uart = UARTService()
 advertisement = ProvideServicesAdvertisement(uart)
 
+# Set RAW=True to see the low-level bytes of data being transfered over BLE
+# using either the UART or Controller modes of the Bluefruit App.
+# Set RAW=False to try an interpret the different data packet formats sent
+# by the Bluefruit App controller mode.
+RAW = False
+
+ButtonNames = ['', '1', '2', '3', '4', 'UP', 'DOWN', 'LEFT', 'RIGHT'];
+
 while True:
     ble.start_advertising(advertisement)
     print("Waiting for the Bluefruit App to connect...")
     while not ble.connected:
         pass
-    print("Bluefruit App is connected: waiting for UART input...")
+    print("Bluefruit App is connected!")
+    if RAW:
+        print("waiting for UART input...")
+    else:
+        print("waiting for Controller input...")
     while ble.connected:
-        # Returns b'' if nothing was read.
-        one_byte = uart.read(1)
-        if one_byte:
-            print(one_byte)
-            uart.write(one_byte)
+        if RAW:
+            # Read a single byte. Returns b'' if nothing was read.
+            one_byte = uart.read(1)
+            if one_byte:
+                # Print and echo the byte.
+                print(one_byte)
+                uart.write(one_byte)
+        else:
+            # Read controller packets.
+            try:
+                packet = Packet.from_stream(uart)
+            except ValueError as e:
+                print('Packet parse error:', e)
+                continue
+            if packet:
+                name = packet.__class__.__name__
+                if isinstance(packet, ColorPacket):
+                    print('Color Packet:', packet.color)
+                elif isinstance(packet, ButtonPacket):
+                    button = ButtonNames[int(packet.button)]
+                    state = 'pressed' if packet.pressed else 'released'
+                    print(f'Button {button} {state}')
+                elif isinstance(packet, (AccelerometerPacket, GyroPacket, MagnetometerPacket)):
+                    print(f'{name}: x={packet.x:.3f} y={packet.y:.3f} z={packet.z:.3f}')
+                elif isinstance(packet, LocationPacket):
+                    print(f'Location: lat={packet.latitude:.3f} lon={packet.longitude:.3f} alt={packet.altitude:.3f}')
+                else:
+                    print(f'Received a new {name}')
+
+    print('Disconnected')
